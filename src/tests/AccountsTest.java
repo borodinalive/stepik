72b205d23612cfec822aec3c9cf4733fee153f55
Alex Borodin <aiborodin@sportmaster.ru>
2018-11-12 16:04:56 +0300

    init git repository

---
+++ src/tests/AccountsTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AccountsTest {
+
+    @Test()
+    @DisplayName("Test not empty accounts")
+    public void testNonEmptyAccounts() {
+
+    }
+
+    @Test()
+    @DisplayName("Test accounts with too much money")
+    public void testAccountsWithTooMuchMoney() {
+    }
+
+
+}
+
